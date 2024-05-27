f4d3f4e4153dd8d553ac7fc517dcd65bec988dba
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-05-27 20:32:35 +0200

    chore: set up the monorepo

---
+++ monorepo-builder.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Config\MBConfig;
+
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->packageDirectories([
+        __DIR__ . '/packages',
+    ]);
+
+    $mbConfig->dataToAppend([
+        ComposerJsonSection::REQUIRE_DEV => [
+            'captainhook/captainhook' => '^5.23',
+            'friendsofphp/php-cs-fixer' => '^3.57',
+            'phpstan/phpstan' => '^1.11',
+            'phpunit/phpunit' => '^11.1',
+            'shipmonk/composer-dependency-analyser' => '^1.5',
+            'symplify/monorepo-builder' => '^11.2',
+        ],
+        ComposerJsonSection::CONFIG => [
+            'sort-packages' => true,
+        ],
+    ]);
+};
